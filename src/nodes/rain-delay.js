6849e2ac6ce0c6239d72aade6069c4e8e0958180
Dan-in-CA <dkimberling@att.net>
2022-12-12 12:40:17 -0800

    Refactor rain-delay.js and rain-delay.html.

--- src/nodes/rain-delay.js
+++ src/nodes/rain-delay.js
@@ -1,27 +1,25 @@
 module.exports = function(RED) {
-    function RainDelayNode(config) {
-        RED.nodes.createNode(this, config);
-        const node = this;
-        var obj = {"sd":"rd", "val":0};
-        node.on('input', function(msg) {
-	        if ("rd_state" in msg.payload) {
-	           if (msg.payload.rd_state == "1" 
-	           	   && parseInt(config.hours) > 0) {
-	             obj.val = parseInt(config.hours);
-	           }
-	           
-	           else if ("val" in msg.payload) { // pass through
-	           node.send(msg)
-	           }
-	           
-	           else if (msg.payload.rd_state == "0") {
-	             obj.val = 0;
-	           } 
-            }                         
-            //if (Number.isNaN(obj.val)) obj.val = 0;
-            var newMsg = {payload: JSON.stringify(obj) }        
-            node.send(newMsg);
-        })
-    }
-    RED.nodes.registerType("sip-rain-delay",RainDelayNode);
+   function RainDelayNode(config) {
+       RED.nodes.createNode(this, config);
+       const node = this;
+       var obj = {"sd":"rd", "val":999};
+       node.on('input', function(msg) {
+	      if ("rd_state" in msg.payload) {
+               if (parseInt(msg.payload.rd_state) === 1) {
+                   if (!("val" in msg.payload)) {
+                       if (config.hours === "" || parseInt(config.hours) == !!0) {
+                           obj.val = -1;
+                       }
+                       else if (parseInt(config.hours) > 0)  obj.val = parseInt(config.hours);
+                   }
+                   else if (Number.isInteger(parseInt(msg.payload.val))) obj.val = parseInt(msg.payload.val)
+               }
+               else if (parseInt(msg.payload.rd_state) === 0) obj.val = 0;
+               else obj.val = -1;
+           }
+           var newMsg = {payload: JSON.stringify(obj) }
+           node.send(newMsg);
+       })
+   }
+   RED.nodes.registerType("sip-rain-delay",RainDelayNode);
 }
