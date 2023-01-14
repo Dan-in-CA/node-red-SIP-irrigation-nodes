bdf2c687bf45a8b8080409a7e0d255c3db146484
Dan-in-CA <dkimberling@att.net>
2023-01-14 11:17:50 -0800

    Refacter run once node.

--- src/nodes/run-once.js
+++ src/nodes/run-once.js
@@ -1,13 +1,36 @@
 module.exports = function(RED) {
    function RunOnceNode(config) {
        RED.nodes.createNode(this, config);
-       const node = this;     
-       node.on('input', function(msg) {    
-           var roObj = JSON.parse('{"ro":[' + msg.payload + ']}');
-           roObj.preempt = config.preempt | 0
-           msg.payload = roObj;
-           node.send(msg);
+       const node = this;
+       node.on('input', function(msg) {
+       	 let roObj = {}
+       	 if (!config.preempt) {
+       	   roObj.preempt = 0
+       	 }
+         if (config.runOnce !== "") {
+           let roLst = config.runOnce.split(/(?<=\])[,]{1}[\s]?/);
+           roLst = roLst.map((item) => JSON.parse(item));
+           roObj.ro = roLst;
+         }
+         if (msg.sipControl !== undefined) {
+           if ("runOnce" in msg.sipControl) {
+             roObj.ro = msg.sipControl.runOnce;
+           }
+           if ("preempt" in msg.sipControl) {
+             roObj.preempt = msg.sipControl.preempt;
+           }              
+         }
+         if (typeof(msg.payload) === "object") {
+           if ("runOnce" in msg.payload) {
+             roObj.ro = msg.payload.runOnce;
+           }
+          if ("preempt" in msg.payload) {
+            roObj.preempt = msg.payload.preempt;
+          }        
+         }
+         msg.payload = roObj;
+         node.send(msg);
        })
-   }    
+   }
    RED.nodes.registerType("sip-run-once", RunOnceNode);
-}  
+};
